00549e0eb8f1a449946b9b4097745123208c8e8a
rajshmachagiri619 <rajesh.machagiri@sakhaglobal.com>
2024-11-28 21:34:39 +0530

    added new code

--- vars/begin.groovy
+++ vars/begin.groovy
@@ -33,6 +33,11 @@ spec:
                         sample.gitclone(url: "https://github.com/rajeshmachagiri/application.git",
                         branch: "main",
                         credsID: "github-app-rajesh-jenkins")
+                        sample.shinvoke("pwd")
+                        sample.shinvoke("ls")
+                        sample.shinvoke("cd /home/jenkins/agent/workspace/main-pipeline")
+                        sample.shinvoke("ls")
+
                     }
                 }
             }
