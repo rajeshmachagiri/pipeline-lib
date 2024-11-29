c6b789573a7e13fdcde2ed3344ae41bdf0799d32
rajshmachagiri619 <rajesh.machagiri@sakhaglobal.com>
2024-11-29 14:29:20 +0530

    added new code

--- vars/begin.groovy
+++ vars/begin.groovy
@@ -47,7 +47,7 @@ spec:
                 steps {
                     script{
                           obj.shellsh("ls ; pwd ")
-                          obj.cd("/application/sample-nodejs/", {npm.npmintall()})
+                          obj.cd("./application/sample-nodejs/", {npm.npmintall()})
 //                        Closure command = {obj.shellsh('echo $PASS')}
 //                        obj.withcreds([credsID: "own-creds",pass: "PASS",user: "USER"], command)
                     }
