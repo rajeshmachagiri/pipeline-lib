b5324355e1fee9c695b38cff373e26c412f830d1
rajshmachagiri619 <rajesh.machagiri@sakhaglobal.com>
2024-11-30 18:23:51 +0530

    new changes

--- vars/begin.groovy
+++ vars/begin.groovy
@@ -54,6 +54,7 @@ spec:
                                          credsID: "github-app-rajesh-jenkins"])
                         String sample1 = obj.libresource("configfile.yaml")
                         conf = obj.readyamlfun(sample1)
+                        prinln(conf["eks"])
 
 
                     }
